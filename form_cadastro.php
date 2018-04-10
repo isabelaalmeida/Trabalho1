9babf08eea441cb569eca760ef144465164cc9a8
isabelaalmeida <isabelaalmeida37@gmail.com>
2018-04-10 06:22:46 -0300

    Cadastro de pessoas

---
+++ form_cadastro.php
@@ -0,0 +1,48 @@
+<!-- Isabela Aparecida -->
+<!DOCTYPE html>
+
+<?php
+		include("cab.inc");
+	?>
+
+<html lang="pt-BR">
+
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Cadastro</title>
+	</head>
+
+<body>
+
+	<form action="index.php" method="post">
+	
+	<p>
+		<label>Nome :</label>
+		<input type="text" name="nome"/>
+
+	</p>
+	
+	<p>
+		<label>Email:</label>
+		<input type="email" name="email"/>
+	</p>
+	
+	<p>
+		<label>Sexo:</label>
+		<option value="F">F</option>
+		<option value="M">M</option>
+	</p>
+	
+	<p>
+		<label>Email:</label>
+		<input type="email" name="email"/>
+	</p>
+	
+
+			
+		</form>
+
+	
+	</body>
+</html>
+						
